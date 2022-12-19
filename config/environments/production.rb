d7099fe4ce1e2e5b1dd8c9130d89b99dde43057a
Emerson Freitas <mersonsi@gmail.com>
2022-12-18 21:52:35 -0300

    feat: ajustes para deploy no fly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,16 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
